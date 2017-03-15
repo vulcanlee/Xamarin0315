153c955b121fff2fc76d57057bfa27099a247e1a
進興 李 <vulcan@miniasp.com>
2017-03-15 11:06:09 +0800

    Xamarin.iOS練習

---
+++ FristXamairniOS/FristXamairniOS/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace FristXamairniOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            OKButton.TouchUpInside += (s, e) =>
+            {
+                lablel.Text = textentry.Text;
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ FristXamairniOS/FristXamairniOS/ViewController.designer.cs
@@ -0,0 +1,46 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace FristXamairniOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lablel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton OKButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textentry { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (lablel != null) {
+                lablel.Dispose ();
+                lablel = null;
+            }
+
+            if (OKButton != null) {
+                OKButton.Dispose ();
+                OKButton = null;
+            }
+
+            if (textentry != null) {
+                textentry.Dispose ();
+                textentry = null;
+            }
+        }
+    }
+}
