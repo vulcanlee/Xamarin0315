c9d7c97a6542da0f9912b03cf48ae44d2665f9c8
進興 李 <vulcan@miniasp.com>
2017-03-15 14:27:44 +0800

    Prism 專案樣板 頁面導航 MVVM

---
+++ PrismNavi/PrismNavi/PrismNavi/ViewModels/NextPageViewModel.cs
@@ -0,0 +1,104 @@
+﻿using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrismNavi.ViewModels
+{
+
+    public class NextPageViewModel : BindableBase, INavigationAware
+    {
+        #region Repositories (遠端或本地資料存取)
+
+        #endregion
+
+        #region ViewModel Property (用於在 View 中作為綁定之用)
+
+        #region echolabel
+        private string _echolabel;
+        /// <summary>
+        /// echolabel
+        /// </summary>
+        public string echolabel
+        {
+            get { return this._echolabel; }
+            set { this.SetProperty(ref this._echolabel, value); }
+        }
+        #endregion
+
+        #endregion
+
+        #region Field 欄位
+
+        public readonly IPageDialogService _dialogService;
+        private readonly INavigationService _navigationService;
+        private readonly IEventAggregator _eventAggregator;
+        #endregion
+
+        #region Constructor 建構式
+        public NextPageViewModel(INavigationService navigationService, IEventAggregator eventAggregator,
+            IPageDialogService dialogService)
+        {
+
+            #region 相依性服務注入的物件
+
+            _dialogService = dialogService;
+            _eventAggregator = eventAggregator;
+            _navigationService = navigationService;
+            #endregion
+
+            #region 頁面中綁定的命令
+
+            #endregion
+
+            #region 事件聚合器訂閱
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Navigation Events (頁面導航事件)
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if(parameters.ContainsKey("echo"))
+            {
+                echolabel = parameters["echo"] as string;
+            }
+            await ViewModelInit();
+        }
+        #endregion
+
+        #region 設計時期或者執行時期的ViewModel初始化
+        #endregion
+
+        #region 相關事件
+        #endregion
+
+        #region 相關的Command定義
+        #endregion
+
+        #region 其他方法
+
+        /// <summary>
+        /// ViewModel 資料初始化
+        /// </summary>
+        /// <returns></returns>
+        private async Task ViewModelInit()
+        {
+            await Task.Delay(100);
+        }
+        #endregion
+
+    }
+}
---
+++ PrismNavi/PrismNavi/PrismNavi/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismNavi.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
