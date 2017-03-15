b094bef365b9c6eb61b8818c4491aac69967211a
進興 李 <vulcan@miniasp.com>
2017-03-15 12:14:06 +0800

    Xamarin.Forms for Prism 開發練習

---
+++ PrismUnityApp1/PrismUnityApp1/PrismUnityApp1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrismUnityApp1.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+        #region entryText
+        private string _entryText;
+        /// <summary>
+        /// entryText
+        /// </summary>
+        public string entryText
+        {
+            get { return this._entryText; }
+            set { this.SetProperty(ref this._entryText, value); }
+        }
+        #endregion
+
+        #region labelText
+        private string _labelText;
+        /// <summary>
+        /// labelText
+        /// </summary>
+        public string labelText
+        {
+            get { return this._labelText; }
+            set { this.SetProperty(ref this._labelText, value); }
+        }
+        #endregion
+
+        public DelegateCommand OKCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            OKCommand = new DelegateCommand(() =>
+            {
+                labelText = entryText;
+            });
+
+            //OKCommand = new DelegateCommand(fooDele);
+        }
+
+        //private void fooDele()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
